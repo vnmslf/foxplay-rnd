a3f4fa7cfc7aab80d631675df0fb73351c48f4fd
miberezkin <vnmslf@gmail.com>
2024-01-22 22:31:56 +0300

    master: list was updated

--- local/templates/foxplay/components/bitrix/news/global__news/bitrix/news.list/.default/result_modifier.php
+++ local/templates/foxplay/components/bitrix/news/global__news/bitrix/news.list/.default/result_modifier.php
@@ -10,6 +10,13 @@ while ($arSection = $rsSections->GetNext()) {
 	$arSections[$arSection['ID']] = $arSection;
 }
 foreach($arResult['ITEMS'] as $arItem) {
+	$date = strtolower(FormatDate("d F Y", MakeTimeStamp($arItem['ACTIVE_FROM'])));
+	$exp = explode(' ', $date);
+	$arItem['day'] = [
+		'number' => $exp[0],
+		'month' => $exp[1],
+		'year' => $exp[2]
+	];
 	$arSections[$arItem['IBLOCK_SECTION_ID']]['ITEMS'][] = $arItem;
 }
 $arResult['SECTIONS'] = $arSections;
--- local/templates/foxplay/components/bitrix/news/global__news/bitrix/news.list/.default/template.php
+++ local/templates/foxplay/components/bitrix/news/global__news/bitrix/news.list/.default/template.php
@@ -13,18 +13,31 @@
 $this->setFrameMode(true);
 ?>
 <div class="listing">
-    <div class="container">
+	<div class="container">
+		<div class="list__blocks">
 <?foreach ($arResult['SECTIONS'] as $arSection) {?>
-        <div class="list__block">
-            <h2><?=$arSection['NAME']?></h2>
-            <ul>
-    <?foreach ($arSection['ITEMS'] as $arItem) {?>
-                <li id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-                    <a href="<?=$arItem['DETAIL_PAGE_URL']?>"><?=$arItem['NAME']?></a>
-                </li>
-    <?}?>
-            </ul>
-        </div>
+			<div class="block">
+				<h2><?=$arSection['NAME']?></h2>
+				<div class="list">
+	<?foreach ($arSection['ITEMS'] as $arItem) {?>
+					<div class="item">
+						<a href="<?=$arItem['DETAIL_PAGE_URL']?>">
+							<span class="day">
+								<span class="day__header"></span>
+								<span class="day__body"><?=$arItem['day']['number']?></span>
+								<span class="day__footer"><?=$arItem['day']['month']?></span>
+							</span>
+							<?=$arItem['NAME']?>
+						</a>
+						<div class="info">
+							<div class="announce"><?=$arItem['PREVIEW_TEXT']?></div>
+							<a href="<?=$arItem['DETAIL_PAGE_URL']?>">Далее</a>
+						</div>
+					</div>
+	<?}?>
+				</div>
+			</div>
 <?}?>
-    </div>
+		</div>
+	</div>
 </div>
